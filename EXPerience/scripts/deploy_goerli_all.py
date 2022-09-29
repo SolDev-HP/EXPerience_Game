e8ede1335dc7779aa40bdd2dc7527a7521faf328
SolDev-HP <develop.hp.block@gmail.com>
2022-09-29 12:38:39 -0400

    Optimism goerli deployment script for EXP ERC20 Token and EXPerience NFT

--- EXPerience/scripts/deploy_goerli_all.py
+++ EXPerience/scripts/deploy_goerli_all.py
@@ -1,4 +1,5 @@
 # Test on goerli before moving on to optimism mainnet
+# Testing on optimism goerlix
 
 import os
 from brownie import EXPToken, EXPerienceNFT, accounts, Contract
@@ -7,77 +8,78 @@ load_dotenv()
 
 def main():
     # Deployer to deploy EXPToken and EXPerience NFT Contract
-    deployer_account = accounts.add(os.getenv("PRIVATE_KEY_DEPLOYER_GOERLI_ADMIN1"))
-    admin2_account = accounts.add(os.getenv("PRIVATE_KEY_DEPLOYER_GOERLI_ADMIN2"))
+    deployer_account = accounts.add(os.getenv("OP_GOERLI_DEPLOYER_PRIV"))
+    admin2_account = accounts.add(os.getenv("OP_GOERLI_ADMIN2_PRIV"))
     # Public hodlers 
-    hodler2 = os.getenv("PUBLIC_KEY_HODLER2_GOERLI")
+    hodler1 = os.getenv("OP_GOERLI_HODLER1_PUB")
+    hodler2 = os.getenv("OP_GOERLI_HODLER2_PUB")
+    hodler3 = os.getenv("OP_GOERLI_HODLER3_PUB")
+    hodler4 = os.getenv("OP_GOERLI_HODLER4_PUB")
+    hodler5 = os.getenv("OP_GOERLI_HODLER5_PUB")
 
     # # Deploy EXPToken ERC20 contract on Goerli
     expContract = EXPToken.deploy("EXPToken", "EXP", {"from": deployer_account})
     # Set another admin for ERC20 contract EXPToken
-    # expContract.setTokenAdmin(os.getenv("PUBLIC_KEY_DEPLOYER_GOERLI_ADMIN2"), True, {"from": deployer_account})
+    expContract.setTokenAdmin(admin2_account.address, True, {"from": deployer_account})
 
     # Distribute
     try: 
-        tx_gain = expContract.gainExperience(hodler2, 24 * 10 ** 18, {"from": deployer_account})
-        tx_gain.wait(1)
+        tx_gain = expContract.gainExperience(hodler1, 24 * 10 ** 18, {"from": deployer_account, "gas_limit": 15 * 10 ** 6, "allow_revert": True})
+        print(tx_gain.info())
+        input("Wait for 2 secs before next one")
+        # hodler2 has no EXP balance and will try to mint EXP NFT
+        # tx_gain2 = expContract.gainExperience(hodler2, 0.56 * 10 ** 18, {"from": deployer_account})
+        # tx_gain2.wait(1)
+        tx_gain3 = expContract.gainExperience(hodler3, 45 * 10 ** 18, {"from": deployer_account, "gas_limit": 15 * 10 ** 6, "allow_revert": True})
+        print(tx_gain3.info())
+        input("Wait for 2 secs before next one")
+        tx_gain4 = expContract.gainExperience(hodler4, 99 * 10 ** 18, {"from": admin2_account, "gas_limit": 15 * 10 ** 6, "allow_revert": True})
+        print(tx_gain4.info())
+        input("Wait for 2 secs before next one")
+        tx_gain5 = expContract.gainExperience(hodler5, 68 * 10 ** 18, {"from": admin2_account, "gas_limit": 15 * 10 ** 6, "allow_revert": True})
+        print(tx_gain5.info())
+        input("Wait for 2 secs before next one")
         # Keeping hodler5 out of this to check 0 EXP balance for NFT
-    except:
-        print("\nDont expect these to fail. but they occasionally do. Dont stop there")
+    except Exception as error_details:
+        print("\nDont expect these to fail. but they occasionally do. Dont stop there\n")
+        print(error_details)
 
     
     exp_token_address = str(expContract)
     input('Verify EXPToken, we are moving to EXPerienceNFT deployment')
-    EXPerienceCon = EXPerienceNFT.deploy("EXPerience NFT", "nEXP", exp_token_address, {"from": deployer_account})
+    exp_nft_contract = EXPerienceNFT.deploy("EXPerience NFT", "nEXP", exp_token_address, {"from": deployer_account})
     
-    # Mint NFT for self - NO EXP balance 
-    # holder1 = accounts.add(os.getenv("PRIVATE_KEY_HODLER1_GOERLI"))
-    # tx_hodler1 = EXPerienceCon.generateExperienceNFT({"from": holder1})     # Shouldn't fail - User has EXP
-    # tx_hodler1.wait(1)
+    # Gas estimations from web3.eth.estamate_gas is failing, hence we need to mention gas limit 
+    # Also allow tx to revert by setting allow_revert 
+    admin1_mint_tx = exp_nft_contract.generateExperienceNFT({"from": deployer_account, "gas_limit": 15 * 10 ** 6, "allow_revert": True})
+    admin1_mint_tx.wait(1)
 
-    # # Has EXPTokens in wallet
-    # holder2 = accounts.add(os.getenv("PRIVATE_KEY_HODLER2_GOERLI"))
-    # tx_hodler2 = EXPerienceCon.generateExperienceNFT({"from": holder2})     # Shouldn't fail - User has EXP
-    # tx_hodler2.wait(1)
-    
-    admin_tx = EXPerienceCon.generateExperienceNFT({"from": deployer_account})
-    admin_tx.wait(1)
+    admin2_mint_tx = exp_nft_contract.generateExperienceNFT({"from": admin2_account, "gas_limit": 15 * 10 ** 6, "allow_revert": True})
+    admin2_mint_tx.wait(1)
 
-    admin2_account = accounts.add(os.getenv("PRIVATE_KEY_DEPLOYER_GOERLI_ADMIN2"))
-    hodl1_tx = EXPerienceCon.generateExperienceNFT({"from": admin2_account})
-    hodl1_tx.wait(1)
+    # self Mint NFT to hodler account
+    holder1 = accounts.add(os.getenv("OP_GOERLI_HODLER1_PRIV"))
+    hodler1_tx = exp_nft_contract.generateExperienceNFT({"from": holder1, "gas_limit": 15 * 10 ** 6, "allow_revert": True})     # Shouldn't fail - User has EXP
+    hodler1_tx.wait(1)
 
-    hodl2acc = accounts.add(os.getenv("PRIVATE_KEY_HODLER1_GOERLI"))
-    hodl2_tx = EXPerienceCon.generateExperienceNFT({"from": hodl2acc})
-    hodl2_tx.wait(1)
+    # Do not have EXP token in wallet, mint operation should work, NFT will display 0 EXP token balance
+    holder2 = accounts.add(os.getenv("OP_GOERLI_HODLER2_PRIV"))
+    hodler2_tx = exp_nft_contract.generateExperienceNFT({"from": holder2, "gas_limit": 15 * 10 ** 6, "allow_revert": True}) 
+    hodler2_tx.wait(1)
 
-    hodl3acc = accounts.add(os.getenv("PRIVATE_KEY_HODLER2_GOERLI"))
-    hodl3_tx = EXPerienceCon.generateExperienceNFT({"from": hodl3acc})
-    hodl3_tx.wait(1)
+    # All of them have EXPTokens in their wallet, this minting operation should work 
+    holder3 = accounts.add(os.getenv("OP_GOERLI_HODLER3_PRIV"))
+    hodler3_tx = exp_nft_contract.generateExperienceNFT({"from": holder3, "gas_limit": 15 * 10 ** 6, "allow_revert": True})     # Shouldn't fail - User has EXP
+    hodler3_tx.wait(1)
 
-    # These will revert, unless we mention gas_limit and allow_revert, brownie wont even broadcast 
-    # the transaction. So let's add those and expect them to fail as only one NFT per wallet is 
-    # allowed 
+    holder4 = accounts.add(os.getenv("OP_GOERLI_HODLER4_PRIV"))
+    hodler4_tx = exp_nft_contract.generateExperienceNFT({"from": holder4, "gas_limit": 15 * 10 ** 6, "allow_revert": True})     # Shouldn't fail - User has EXP
+    hodler4_tx.wait(1)
 
-    admin_tx_rev = EXPerienceCon.generateExperienceNFT({"from": deployer_account, "gas_limit": 150000, "allow_revert": True})
-    admin_tx_rev.wait(1)
+    holder5 = accounts.add(os.getenv("OP_GOERLI_HODLER5_PRIV"))
+    hodler5_tx = exp_nft_contract.generateExperienceNFT({"from": holder5, "gas_limit": 15 * 10 ** 6, "allow_revert": True})     # Shouldn't fail - User has EXP
+    hodler5_tx.wait(1)
 
-    hodl1_tx_rev = EXPerienceCon.generateExperienceNFT({"from": admin2_account, "gas_limit": 150000, "allow_revert": True})
-    hodl1_tx_rev.wait(1)
-
-    hodl2_tx_rev = EXPerienceCon.generateExperienceNFT({"from": hodl2acc, "gas_limit": 150000, "allow_revert": True})
-    hodl2_tx_rev.wait(1)
-
-    hodl3_tx_rev = EXPerienceCon.generateExperienceNFT({"from": hodl3acc, "gas_limit": 150000, "allow_revert": True})
-    hodl3_tx_rev.wait(1)
-    # admin2_tx = EXPerienceCon.generateExperienceNFT({"from": admin2_account})
-    # admin2_tx.wait(1)
-    # Check if one per address works 
-    # try: 
-    #     holder_remint = EXPerienceCon.generateExperienceNFT({"from": deployer_account})
-    #     holder_remint.wait(1)
-    #     # holder2_remint = expContract.gainExperience({"from": holder2})
-    #     # holder2_remint.wait(1)
-    #     # Keeping hodler5 out of this to check 0 EXP balance for NFT
-    # except:
-    #     print("\nThis has to fail. It's expected")
+    # This should fail with the condition that only one per wallet mint is allowed 
+    hodler5_tx_re = exp_nft_contract.generateExperienceNFT({"from": holder5, "gas_limit": 15 * 10 ** 6, "allow_revert": True})     # Should fail, user already has EXP NFT and trying to mint again
+    hodler5_tx_re.wait(1)
